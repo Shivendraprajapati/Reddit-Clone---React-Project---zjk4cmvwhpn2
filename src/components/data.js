bbd10da2aa7d36f22b3411c739b9835d9a64b806
Shivendraprajapati <111180105+Shivendraprajapati@users.noreply.github.com>
2023-02-23 14:52:17 +0530

    Delete src directory

--- src/components/data.js
+++
@@ -1,9 +0,0 @@
-export const posts = [
-    {
-        postedBy: "Kunal",
-        postText:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        upVotes:3,
-				downVotes:1
-    },
-];
